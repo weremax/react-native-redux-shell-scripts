73f9b874c8e57cc9c0c1ecd50b35b2b17ac1db16
Christopher Blankenship <hardcode45@yahoo.com>
2018-01-21 19:12:37 -0500

    updated installer

--- installer.sh
+++ installer.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-npm install --save-dev eslint-config-rallycoding
-
-npm install --save react-native-router-flux
-npm install --save react-native-vector-icons
-npm install --save react-redux redux redux-thunk
+#npm install --save react-native-router-flux react-native-vector-icons react-redux redux redux-thunk
 
 # mkdir
 mkdir ./src
